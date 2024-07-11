0731c539d520a0768bab187d1cd44c7cb3bb2268
amit <amitsarker95@gmail.com>
2024-07-11 23:12:05 +0600

    mysql database connected

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -68,8 +68,13 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'expense_tracker',
+        'USER': 'amit',
+        'PASSWORD': 'amitsarker95@',
+        'HOST': 'localhost',   
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
--- expenses/models.py
+++ expenses/models.py
@@ -18,6 +18,7 @@ class Category(models.Model):
 
 class Expense(models.Model):
 
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='exenses', on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
@@ -25,4 +26,4 @@ class Expense(models.Model):
     date = models.DateField()
 
     def __str__(self):
-        return f"{self.title} - {self.amount}"
+        return f"{self.user.first_name} {self.title} - {self.amount}"
