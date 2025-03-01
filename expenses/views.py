3805253f4e334abb193f86525a4c55ee676b5185
amit <amitsarker95@gmail.com>
2025-03-01 23:36:53 +0600

    add post request in IncomeView

--- expenses/views.py
+++ expenses/views.py
@@ -17,6 +17,12 @@ class IncomeViewSet(APIView):
         incomes = Income.objects.all()
         serializer = IncomeSerializer(incomes, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = IncomeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
 
 
 class ExpenseViewSet(ModelViewSet):
