e340a20e1c022c7d4432417f5f3aa9e2545994b3
amit <amitsarker95@gmail.com>
2025-03-01 23:54:55 +0600

    added IncomeDetaileView

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,17 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ExpenseViewSet, CategoryViewSet, IncomeViewSet, BudgetViewSet, \
+from .views import ExpenseViewSet, CategoryViewSet, IncomeViewSet,IncomeDetailViewSet, BudgetViewSet, \
     RecurringExpenseViewSet, SavingsGoalViewSet
 
 router = DefaultRouter()
 
 router.register('expense', ExpenseViewSet, basename='expenses')
 router.register('category', CategoryViewSet, basename='categories')
-router.register('income', IncomeViewSet, basename='income')
+# router.register('income', IncomeViewSet, basename='income')
 router.register('budget', BudgetViewSet, basename='budget')
 router.register('recurring', RecurringExpenseViewSet, basename='recurring')
 router.register('savings', SavingsGoalViewSet, basename='savings')
 urlpatterns = [
+
     path('', include(router.urls)),
+    path('income/', IncomeViewSet.as_view(), name='income'),
+    path('income/<int:pk>/', IncomeDetailViewSet.as_view(), name='income-detail'),
     
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import ExpenseSerializer, CategorySerializer, \
     IncomeSerializer, BudgetSerializer , RecurringExpenseSerializer,\
@@ -16,13 +18,28 @@ class IncomeViewSet(APIView):
     def get(self, request):
         incomes = Income.objects.all()
         serializer = IncomeSerializer(incomes, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request):
         serializer = IncomeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+class IncomeDetailViewSet(APIView):
+    serializer_class = IncomeSerializer
+    def get(self, request,pk):
+        income = Income.objects.get(pk=pk)
+        serializer = IncomeSerializer(income)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk=None):
+        income = get_object_or_404(income, pk=pk)
+        serializer = IncomeSerializer(income, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class ExpenseViewSet(ModelViewSet):
