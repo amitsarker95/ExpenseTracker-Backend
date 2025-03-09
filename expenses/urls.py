9312ed821f8c4863d0ed6b99d957a4d1372b21c7
amit <amitsarker95@gmail.com>
2025-03-09 14:06:46 +0600

    make some change in urls

--- expenses/urls.py
+++ expenses/urls.py
@@ -6,11 +6,7 @@ from .views import ExpenseViewSet, CategoryViewSet, IncomeViewSet,IncomeDetailVi
 router = DefaultRouter()
 
 router.register('expense', ExpenseViewSet, basename='expenses')
-# router.register('category', CategoryViewSet, basename='categories')
-# router.register('income', IncomeViewSet, basename='income')
-# router.register('budget', BudgetViewSet, basename='budget')
 router.register('recurring', RecurringExpenseViewSet, basename='recurring')
-# router.register('savings', SavingsGoalViewSet, basename='savings')
 urlpatterns = [
 
     path('', include(router.urls)),
